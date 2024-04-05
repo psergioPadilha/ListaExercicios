53897ea6732fdd8cda5151827d71a8d964144eb3
psergioPadilha <psergiopadilha@gmail.com>
2024-04-05 19:59:00 -0300

    05/04/2024

--- Exercicio03.ConsoleApp/Program.cs
+++ Exercicio03.ConsoleApp/Program.cs
@@ -2,6 +2,8 @@
 //V = PI * raio² x altura
 
 
+using System.ComponentModel.Design;
+
 namespace Exercicio03.ConsoleApp
 {
     internal class Program
@@ -10,26 +12,108 @@ namespace Exercicio03.ConsoleApp
         {
             while (true)
             {
-                Console.Clear();
-                Console.WriteLine("PROGRAMA CALCULA VOLUME DO CILINDRO");
+                double raio = 0;
+                double altura = 0;
 
-                Console.Write("Raio do cilindro: ");
-                double raio = Convert.ToDouble(Console.ReadLine());
+                string opcao = Menu();
 
-                Console.Write("Altura do cilindro: ");
-                double altura = Convert.ToDouble(Console.ReadLine());
+                AnalizaOpcaoDeMenu(opcao);
+                
+                LerDadosDeEntrada(ref raio, ref altura);
 
-                double volume = calculaVolumeCilindrico(raio, altura);
+                double volume = CalculaVolumeCilindrico(raio, altura);
 
-                Console.WriteLine("O volume do cilindro é " + volume);
+                ApresentaVolume(volume);
 
-                Console.ReadLine();
             }
 
-            static double calculaVolumeCilindrico(double r, double a)
+            #region Menu
+            static string Menu() {
+                Console.Clear();
+                Console.WriteLine("PROGRAMA CALCULA VOLUME DO CILINDRO");
+                Console.WriteLine();
+                Console.WriteLine("Digite 'S' para sair e 'C' para continuar!");
+                Console.Write("Opção: ");
+                string opcao = Console.ReadLine();
+                return opcao;
+            }
+            #endregion
+
+            #region Analiza opção do menu
+            static void AnalizaOpcaoDeMenu(string opcao)
+            {
+                while( (opcao != "S") && (opcao != "s") && (opcao != "C") && (opcao != "c"))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Opção inválida...");
+                    Console.Write("Opção: ");
+                    opcao = Console.ReadLine();
+                }
+                if ((opcao == "S") || (opcao == "s"))
+                {
+                    if (ConfirmaSaida())
+                        Environment.Exit(0);
+                }
+            }
+            #endregion
+
+            #region Confirmação para sair do sistema
+            static bool ConfirmaSaida()
+            {
+                Console.WriteLine();
+                Console.WriteLine("Deseja mesmo encerrar o sistema?");
+                Console.WriteLine("Digite 'S' para sair e 'C' para continuar!");
+                Console.Write("Opção: ");
+                string opcao = Console.ReadLine();
+
+                while ((opcao != "S") && (opcao != "s") && (opcao != "C") && (opcao != "c"))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Opção inválida...");
+                    Console.WriteLine("Digite 'S' para sair e 'C' para continuar!");
+                    Console.Write("Opção: ");
+                    opcao = Console.ReadLine();
+                }
+                if ( (opcao == "S") || (opcao == "s") )
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            #endregion
+
+            #region Ler dados de entrada
+            static void LerDadosDeEntrada(ref double raio, ref double altura)
+            {
+                Console.Clear();
+                Console.WriteLine("PROGRAMA CALCULA VOLUME DO CILINDRO");
+                Console.WriteLine();
+                Console.Write("Raio do cilindro: ");
+                raio = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Altura do cilindro: ");
+                altura = Convert.ToDouble(Console.ReadLine());
+            }
+            #endregion
+
+            #region Calcula Volume do cilindro
+            static double CalculaVolumeCilindrico(double r, double a)
             {
                 return Math.PI * Math.Pow(r, 2) * a;
             }
+            #endregion
+
+            #region Apresenta calculo do volume
+            static void ApresentaVolume(double volume)
+            {
+                Console.WriteLine("O volume do cilindro é " + volume.ToString("N4"));
+
+                Console.ReadLine();
+            }
+            #endregion
         }
     }
 }
--- Exercicio05.ConsoleApp/Program.cs
+++ Exercicio05.ConsoleApp/Program.cs
@@ -9,24 +9,120 @@ namespace Exercicio05.ConsoleApp
         {
             while (true)
             {
-                Console.Clear();
-                Console.WriteLine("PROGRAMA CALCULA VOLUME DE ESFERA");
-                Console.WriteLine();
+                string opcao = Menu();
 
-                Console.Write("Raio: ");
-                double raio = Convert.ToDouble(Console.ReadLine());
+                if(VerificaOpcaoDeMenu(opcao))
+                    Environment.Exit(0);
 
-                double volume = calculaVolumeDaEsfera(raio);
+                double raio = LerRaio();
 
-                Console.WriteLine("O volume da esfera é " + volume.ToString("F"));
+                double volume = CalculaVolumeDaEsfera(raio);
 
-                Console.ReadLine();
+                ApresentaVolumeDaEsfera(volume);
             }
         }
 
-        static double calculaVolumeDaEsfera(double r)
+        #region Menu
+        static string Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("PROGRAMA CALCULA VOLUME DE ESFERA");
+            Console.WriteLine();
+            Console.WriteLine("Digite 'S' para sair e 'C' para continuar!");
+            Console.Write("Opção: ");
+            string opcao = Console.ReadLine();
+            return opcao;
+        }
+        #endregion
+
+        #region Verifica opção de menu
+        static bool VerificaOpcaoDeMenu(string opcao)
+        {
+            while( (opcao != "S") && (opcao != "s") && (opcao != "C") && (opcao != "c") )
+            {
+                Console.WriteLine();
+                Console.WriteLine("Opção inválida digite novamente!");
+                Console.Write("Opção: ");
+                opcao = Console.ReadLine();
+            }
+            if ( (opcao == "S") || (opcao == "s") )
+            {
+                if(ConfirmaSairDoSistema())
+                    return true;
+                else return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        #endregion
+
+        #region Confirmação para sair do sistema
+        static bool ConfirmaSairDoSistema()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Deseja mesmo sair do sistema?");
+            Console.WriteLine("Digite 'S' para sair e 'C' para continuar!");
+            Console.Write("Opção: ");
+            string opcao = Console.ReadLine();
+
+            while ( (opcao != "S") && ( opcao != "s") && (opcao != "C") && (opcao != "c"))
+            {
+                Console.WriteLine();
+                Console.WriteLine("Opção inválida...");
+                Console.WriteLine();
+                Console.WriteLine("Digite 'S' para sair e 'C' para continuar!");
+                Console.Write("Opção: ");
+                opcao = Console.ReadLine();
+            }
+            if ( (opcao == "S") || (opcao == "s") )
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        #endregion
+
+        #region Ler raio
+        static double LerRaio()
+        {
+            Console.WriteLine();
+            Console.Write("Raio: ");
+            string valor = Console.ReadLine();
+            double raio = VerificaEntrada(valor);
+            return raio;
+        }
+        #endregion
+
+        static double VerificaEntrada(string valor)
+        {
+            while (!double.TryParse(valor, out var r))
+            {
+                Console.WriteLine("Valor inválido por favor digite novamente!");
+                Console.Write("Raio: ");
+                valor = Console.ReadLine();
+            }
+            return double.Parse(valor);
+        }
+
+        #region Calculo do volume da esfera
+        static double CalculaVolumeDaEsfera(double r)
         {
             return 4 /3 * Math.PI * Math.Pow(r,3);
         }
+        #endregion
+
+        #region
+        static void ApresentaVolumeDaEsfera(double volume)
+        {
+            Console.WriteLine();
+            Console.WriteLine("O volume da esfera é " + volume.ToString("F"));
+            Console.ReadLine();
+        }
+        #endregion
     }
 }
