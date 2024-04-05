6e8fd01e6221535dc6cb0496029372fa9f023c31
psergioPadilha <psergiopadilha@gmail.com>
2024-04-05 20:14:05 -0300

    05/06/2024

--- Exercicio03.ConsoleApp/Program.cs
+++ Exercicio03.ConsoleApp/Program.cs
@@ -27,93 +27,94 @@ namespace Exercicio03.ConsoleApp
 
             }
 
-            #region Menu
-            static string Menu() {
-                Console.Clear();
-                Console.WriteLine("PROGRAMA CALCULA VOLUME DO CILINDRO");
-                Console.WriteLine();
-                Console.WriteLine("Digite 'S' para sair e 'C' para continuar!");
-                Console.Write("Opção: ");
-                string opcao = Console.ReadLine();
-                return opcao;
-            }
-            #endregion
-
-            #region Analiza opção do menu
-            static void AnalizaOpcaoDeMenu(string opcao)
-            {
-                while( (opcao != "S") && (opcao != "s") && (opcao != "C") && (opcao != "c"))
-                {
-                    Console.WriteLine();
-                    Console.WriteLine("Opção inválida...");
-                    Console.Write("Opção: ");
-                    opcao = Console.ReadLine();
-                }
-                if ((opcao == "S") || (opcao == "s"))
-                {
-                    if (ConfirmaSaida())
-                        Environment.Exit(0);
-                }
-            }
-            #endregion
-
-            #region Confirmação para sair do sistema
-            static bool ConfirmaSaida()
-            {
-                Console.WriteLine();
-                Console.WriteLine("Deseja mesmo encerrar o sistema?");
-                Console.WriteLine("Digite 'S' para sair e 'C' para continuar!");
-                Console.Write("Opção: ");
-                string opcao = Console.ReadLine();
-
-                while ((opcao != "S") && (opcao != "s") && (opcao != "C") && (opcao != "c"))
-                {
-                    Console.WriteLine();
-                    Console.WriteLine("Opção inválida...");
-                    Console.WriteLine("Digite 'S' para sair e 'C' para continuar!");
-                    Console.Write("Opção: ");
-                    opcao = Console.ReadLine();
-                }
-                if ( (opcao == "S") || (opcao == "s") )
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            #endregion
-
-            #region Ler dados de entrada
-            static void LerDadosDeEntrada(ref double raio, ref double altura)
-            {
-                Console.Clear();
-                Console.WriteLine("PROGRAMA CALCULA VOLUME DO CILINDRO");
-                Console.WriteLine();
-                Console.Write("Raio do cilindro: ");
-                raio = Convert.ToDouble(Console.ReadLine());
-
-                Console.Write("Altura do cilindro: ");
-                altura = Convert.ToDouble(Console.ReadLine());
-            }
-            #endregion
-
-            #region Calcula Volume do cilindro
-            static double CalculaVolumeCilindrico(double r, double a)
-            {
-                return Math.PI * Math.Pow(r, 2) * a;
-            }
-            #endregion
-
-            #region Apresenta calculo do volume
-            static void ApresentaVolume(double volume)
-            {
-                Console.WriteLine("O volume do cilindro é " + volume.ToString("N4"));
-
-                Console.ReadLine();
-            }
-            #endregion
         }
+        #region Menu
+        static string Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("PROGRAMA CALCULA VOLUME DO CILINDRO");
+            Console.WriteLine();
+            Console.WriteLine("Digite 'S' para sair e 'C' para continuar!");
+            Console.Write("Opção: ");
+            string opcao = Console.ReadLine();
+            return opcao;
+        }
+        #endregion
+
+        #region Analiza opção do menu
+        static void AnalizaOpcaoDeMenu(string opcao)
+        {
+            while ((opcao != "S") && (opcao != "s") && (opcao != "C") && (opcao != "c"))
+            {
+                Console.WriteLine();
+                Console.WriteLine("Opção inválida...");
+                Console.Write("Opção: ");
+                opcao = Console.ReadLine();
+            }
+            if ((opcao == "S") || (opcao == "s"))
+            {
+                if (ConfirmaSaida())
+                    Environment.Exit(0);
+            }
+        }
+        #endregion
+
+        #region Confirmação para sair do sistema
+        static bool ConfirmaSaida()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Deseja mesmo encerrar o sistema?");
+            Console.WriteLine("Digite 'S' para sair e 'C' para continuar!");
+            Console.Write("Opção: ");
+            string opcao = Console.ReadLine();
+
+            while ((opcao != "S") && (opcao != "s") && (opcao != "C") && (opcao != "c"))
+            {
+                Console.WriteLine();
+                Console.WriteLine("Opção inválida...");
+                Console.WriteLine("Digite 'S' para sair e 'C' para continuar!");
+                Console.Write("Opção: ");
+                opcao = Console.ReadLine();
+            }
+            if ((opcao == "S") || (opcao == "s"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        #endregion
+
+        #region Ler dados de entrada
+        static void LerDadosDeEntrada(ref double raio, ref double altura)
+        {
+            Console.Clear();
+            Console.WriteLine("PROGRAMA CALCULA VOLUME DO CILINDRO");
+            Console.WriteLine();
+            Console.Write("Raio do cilindro: ");
+            raio = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Altura do cilindro: ");
+            altura = Convert.ToDouble(Console.ReadLine());
+        }
+        #endregion
+
+        #region Calcula Volume do cilindro
+        static double CalculaVolumeCilindrico(double r, double a)
+        {
+            return Math.PI * Math.Pow(r, 2) * a;
+        }
+        #endregion
+
+        #region Apresenta calculo do volume
+        static void ApresentaVolume(double volume)
+        {
+            Console.WriteLine("O volume do cilindro é " + volume.ToString("N4"));
+
+            Console.ReadLine();
+        }
+        #endregion
     }
 }
