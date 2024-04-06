939888fe8f3d1a556cd0f903238811a114430e14
psergioPadilha <psergiopadilha@gmail.com>
2024-04-05 23:41:20 -0300

    05/04/2024

--- Exercicio07.ConsoleApp/Program.cs
+++ Exercicio07.ConsoleApp/Program.cs
@@ -1,30 +1,62 @@
 ﻿//Crie um programa para calcular o salário total de um vendedor
 //Deverá ser informado o salário base e o total de vendas. A comissão é calculada com um percentual
 //(informado pelo usuário) sobre o total de vendas.
+//Saláriototal = saláriobase + (totaldevendas * (comissão/100))
 
 
 namespace Exercicio07.ConsoleApp
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
-            decimal salarioBase, totalVendas, salarioTotal, percentual;
+            while (true)
+            {
+                Console.Clear();
 
-            Console.WriteLine("Programa calcula salário");
+                double salarioBase = 0;
+                double totalDeVendas = 0;
+                double salarioTotal = 0;
+                int comissao = 0;
 
-            Console.WriteLine("Digite o salário base:");
-            salarioBase = Convert.ToInt32(Console.ReadLine());
+                LerEntradasDoUsuario(ref salarioBase, ref totalDeVendas, ref comissao);
 
-            Console.WriteLine("Digite o total de vendas:");
-            totalVendas = Convert.ToInt32(Console.ReadLine());
+                salarioTotal = CalcularSalarioTotal(salarioBase, totalDeVendas, comissao);
 
-            Console.WriteLine("Digite o percentual de comissionamento:");
-            percentual = Convert.ToInt32(Console.ReadLine());
+                ImprimirSalarioTotal(salarioTotal);
 
-            salarioTotal = salarioBase + (totalVendas * percentual / 100);
-
-            Console.WriteLine("O salário total é: " +  salarioTotal);
+                Console.ReadLine();
+            }
         }
+
+        #region Informações de entrada
+        static void LerEntradasDoUsuario(ref double salarioBase, ref double totalDeVendas, ref int comissao)
+        {
+            Console.Write("Salário base: ");
+            salarioBase = double.Parse(Console.ReadLine());
+
+            Console.Write("Total de vendas: ");
+            totalDeVendas = double.Parse(Console.ReadLine());
+
+            Console.Write("Percentual de comissionamento: ");
+            comissao = int.Parse(Console.ReadLine());
+        }
+        #endregion
+
+        #region Calcula o salário final
+        static double CalcularSalarioTotal(double salarioBase, double totalDeVendas, int comissao)
+        {
+            double salrioTotal = salarioBase + (totalDeVendas * comissao / 100);
+            return salrioTotal;
+        }
+        #endregion
+
+        #region Imprime o salário final
+        static void ImprimirSalarioTotal(double salarioTotal)
+        {
+            Console.WriteLine("O salário final é R$" +  salarioTotal + ".");
+        }
+        #endregion
     }
 }
